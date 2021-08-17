d05ab99e3e2da283d4202f95a4463066ce1feaa3
SandrineMuhi <muhimpundusandrine2016@gmail.com>
2021-08-16 20:50:02 -0400

    Adding queries.sql,schema.sql and ERD schema.txt

--- schema.sql
+++ schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_manager (
+CREATE TABLE manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
@@ -33,6 +33,25 @@ CREATE TABLE salaries (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+	title varchar NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (from_date)
+);
+
+CREATE TABLE dept_Emp (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (dept_no, emp_no)
 );
 
 SELECT * FROM departments;
