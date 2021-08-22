1aff26e6b70e4cff816c830345cfb455f66732e7
SandrineMuhi <muhimpundusandrine2016@gmail.com>
2021-08-22 01:42:22 -0400

    Adding images

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -39,24 +39,20 @@ FROM unique_titles
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
 
---Deliverable 2: Mentorship eligibility
-SELECT DISTINCT ON(em.emp_no)em.emp_no,
-em.first_name,
-em.last_name,
-em.birth_date,
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
 de.from_date,
 de.to_date,
 ti.title
 --INTO mentorship_eligibility
-FROM employees AS em
+FROM employees AS e
 INNER JOIN dept_emp AS de
-ON (em.emp_no = de.emp_no)
-INNER JOIN titles AS ti
-ON (em.emp_no = ti.emp_no)
-WHERE (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-AND(ti.to_date ='9999-01-01')
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND(de.to_date ='9999-01-01')
 ORDER BY emp_no;
-
-
-
 
